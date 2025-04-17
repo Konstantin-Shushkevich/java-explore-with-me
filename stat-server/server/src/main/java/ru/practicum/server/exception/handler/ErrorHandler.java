2c9dfdda6551564ebf71aa76a82174682def8206
Konstantin Shushkevich <shusha113@yandex.ru>
2025-04-17 21:09:31 +0300

    Dp part2 fix12. Final fix

--- stat-server/server/src/main/java/ru/practicum/server/exception/handler/ErrorHandler.java
+++ stat-server/server/src/main/java/ru/practicum/server/exception/handler/ErrorHandler.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import ru.practicum.server.exception.DateTimeNotValidException;
 
 @Slf4j
 @RestControllerAdvice
@@ -12,6 +13,13 @@ public class ErrorHandler {
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleDateTimeNotValidException(final DateTimeNotValidException e) {
+        log.error("DateTimeNotValidException was thrown");
+        return new ErrorResponse("Incorrect time of start or end", e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorResponse handleException(final Exception e) {
         log.error("Exception was thrown");
         return new ErrorResponse("Something went wrong", e.getMessage());
